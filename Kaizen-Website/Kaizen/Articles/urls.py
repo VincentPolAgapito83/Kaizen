cddb7fd6536662e9237ed9bcf66f6b7cc61ae069
Qu1n4n <qrlquinanjr@tip.edu.ph>
2023-12-17 17:10:19 +0800

    second commit

--- Kaizen-Website/Kaizen/Articles/urls.py
+++ Kaizen-Website/Kaizen/Articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from .views  import HomePageView, SearchResultsView
 
 urlpatterns = [
+    path('register/', views.registration, name="register"),
     path('search/', SearchResultsView.as_view(), name="search_results"),
     path('', HomePageView.as_view(), name="home"),
 ]
--- Kaizen-Website/Kaizen/Articles/views.py
+++ Kaizen-Website/Kaizen/Articles/views.py
@@ -2,9 +2,25 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView, ListView
 from .models import Articles
 from django.db.models import Q
+
+
+def registration(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            
+    context = {'form':form}
+    return render(request, 'members/registration.html', context)
+
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
