39a2ba0738512a5c4d81f298c144ef242439391c
Qu1n4n <qrlquinanjr@tip.edu.ph>
2023-12-22 16:16:58 +0800

    Sixth Commit

--- Kaizen-Website/Kaizen/Articles/urls.py
+++ Kaizen-Website/Kaizen/Articles/urls.py
@@ -3,7 +3,9 @@ from . import views
 from .views  import HomePageView, SearchResultsView
 
 urlpatterns = [
-    path('register/', views.registration, name="register"),
+    path('register/', views.user_registration, name="register"),
+    path('login/', views.user_login, name="login"),
     path('search/', SearchResultsView.as_view(), name="search_results"),
+    path('search/', views.search_bar, name="search-bar"),
     path('', HomePageView.as_view(), name="home"),
 ]
--- Kaizen-Website/Kaizen/Articles/views.py
+++ Kaizen-Website/Kaizen/Articles/views.py
@@ -3,23 +3,46 @@ from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.views.generic import TemplateView, ListView
 from .models import Articles
 from django.db.models import Q
 
+def search_bar(request):
+    if request.method == "POST":
+        results = request.POST['results']
+        return render(request, 'searchbar.html',
+                       {'results':results}) 
+    else:
+        return render(request, 'searchbar.html', {})
 
-def registration(request):
+def user_registration(request):
     form = UserCreationForm()
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            user = form.cleaned_data.get('username')
+            user = form.cleaned_data.get('email')
+            messages.success(request, 'Account Created Successfully')
             
     context = {'form':form}
-    return render(request, 'members/registration.html', context)
+    return render(request, 'members/registration2.html', context)
 
+def user_login(request):
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, email)
+
+    context = {}
+    return render(request, 'members/login2.html', context)
 
 
 class HomePageView(TemplateView):
